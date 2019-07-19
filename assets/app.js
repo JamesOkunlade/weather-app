1167870477cc0e1f13bd166593d5498a10c50224
James Okunlade <james.okunlade@gmail.com>
2019-07-19 19:12:20 +0100

    fetch api for rainy and sunny day background gif added

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,30 @@
 
+// Background url for Rainy day
+
+function rainyDay() {
+  fetch('https://api.giphy.com/v1/gifs/translate?api_key=fHN5h5cQ7aCvqp1t7pGeGhZ54gV67MtW&s=rain', {mode: 'cors'})
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(response) {
+      return response.data.images.original.url;
+    })
+    .catch(e => {
+      console.log(e);
+    })
+}
+
+// Background url Sunny day
+
+function rainyDay() {
+  fetch('https://api.giphy.com/v1/gifs/translate?api_key=fHN5h5cQ7aCvqp1t7pGeGhZ54gV67MtW&s=sun', {mode: 'cors'})
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(response) {
+      return response.data.images.original.url;
+    })
+    .catch(e => {
+      console.log(e);
+    })
+}
