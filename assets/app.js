a7dd9143502f6a92769844668b38eafc5501cfe0
James Okunlade <james.okunlade@gmail.com>
2019-07-20 03:41:00 +0100

    weather app basic functionality completed

--- assets/app.js
+++ assets/app.js
@@ -1,48 +1,67 @@
-// Background url for Rainy day
-
 
 
 window.addEventListener('load', () => {
   let long;
   let lat;
+  let city;
+  let country;
   let currentLocation = false;
+  const cityRegEx = /(?<city>\w+)/;
+  const countryRegEx = /(?<country>[^\s]*$)/;
 
-  // For allowing us to make bypass cors protocol by making request through a localhost
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
 
   // Accessing the DOM
   const content = document.querySelector('#content');
   const temperature = document.querySelector('.temperature');
-
-
-  // Getting the coordinate of current location
-  navigator.geolocation.getCurrentPosition(success);
-
-  function success(position) {
-    const coords = position.coords
-    lat = coords.latitude;
-    long = coords.longitude;
-    console.log(`Lat: ${lat}, Long: ${long}`);
-    currentLocation = true;
-  }
+  const desc = document.querySelector('.description');
+  const hum = document.querySelector('.humidity');
+  const loc = document.querySelector('.location');
 
 
 
 
-  const api = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8b9fb0f997204e2487c49916e25f3c18'
+
+  document.querySelector('#search-form').addEventListener('submit', function(e)
+  {
+    e.preventDefault();
+    const location = document.querySelector('#location').value;
+    console.log(location);
+
+    // Extracting city and country out of string
+    city = location.match(cityRegEx).groups.city;
+    country = location.match(countryRegEx).groups.country;
+    console.log(city);
+    console.log(country);
+
+    // Clearing the input field after submit
+    document.querySelector('#location').value = '';
+
+    // Generate api url with city and country name
+    const api = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=8b9fb0f997204e2487c49916e25f3c18`
+
+    // Fetching data via the api
+    fetch(api, {mode: 'cors'})
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        const {temp, humidity} = data.main;
+        const {description} = data.weather[0];
+        temperature.textContent = `${temp}K`;
+        desc.textContent = `${description}`;
+        hum.textContent = `Humidity: ${humidity}`;
+        loc.textContent = `${location}`
+      })
+      .catch(e => {
+        console.log(e);
+      })
 
 
-  fetch(api, {mode: 'cors'})
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      const {temp, humidity} = data.main;
-      temperature.textContent = `${temp}K`;
-    })
-    .catch(e => {
-      console.log(e);
-    })
+
+  });
+
+
+
 
 })
