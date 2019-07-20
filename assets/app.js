862bf405353bd49c03e9f31c2141bb04240d5508
James Okunlade <james.okunlade@gmail.com>
2019-07-20 01:01:13 +0100

    fetch to open weather api implemented and changes to html styling

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,48 @@
-
 // Background url for Rainy day
 
-function rainyDay() {
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=fHN5h5cQ7aCvqp1t7pGeGhZ54gV67MtW&s=rain', {mode: 'cors'})
-    .then(function(response) {
-      return response.json()
+
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  let currentLocation = false;
+
+  // For allowing us to make bypass cors protocol by making request through a localhost
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+  // Accessing the DOM
+  const content = document.querySelector('#content');
+  const temperature = document.querySelector('.temperature');
+
+
+  // Getting the coordinate of current location
+  navigator.geolocation.getCurrentPosition(success);
+
+  function success(position) {
+    const coords = position.coords
+    lat = coords.latitude;
+    long = coords.longitude;
+    console.log(`Lat: ${lat}, Long: ${long}`);
+    currentLocation = true;
+  }
+
+
+
+
+  const api = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8b9fb0f997204e2487c49916e25f3c18'
+
+
+  fetch(api, {mode: 'cors'})
+    .then(response => {
+      return response.json();
     })
-    .then(function(response) {
-      return response.data.images.original.url;
+    .then(data => {
+      console.log(data);
+      const {temp, humidity} = data.main;
+      temperature.textContent = `${temp}K`;
     })
     .catch(e => {
       console.log(e);
     })
-}
 
-// Background url Sunny day
-
-function rainyDay() {
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=fHN5h5cQ7aCvqp1t7pGeGhZ54gV67MtW&s=sun', {mode: 'cors'})
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(response) {
-      return response.data.images.original.url;
-    })
-    .catch(e => {
-      console.log(e);
-    })
-}
+})
