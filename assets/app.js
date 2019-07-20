431e5ccfe572c6f5b8e71117269704ec99a6ff8a
James Okunlade <james.okunlade@gmail.com>
2019-07-20 13:01:41 +0100

    Today's date added to weather details

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,26 @@
 
 
 window.addEventListener('load', () => {
-  let long;
-  let lat;
   let city;
   let country;
-  let currentLocation = false;
   const cityRegEx = /(?<city>\w+)/;
   const countryRegEx = /(?<country>[^\s]*$)/;
   const date = Date.now();
+
+  // Formatting today's date
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //January is 0!
+  let yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  
+  today = dd + '/' + mm + '/' + yyyy;
 
 
   // Accessing the DOM
@@ -31,55 +43,51 @@ window.addEventListener('load', () => {
     loc.textContent = `${location}`
 
     // Extracting city and country out of string
-    city = location.match(cityRegEx).groups.city;
-    country = location.match(countryRegEx).groups.country;
-    console.log(city);
-    console.log(country);
-
-    // Clearing the input field after submit
-    document.querySelector('#location').value = '';
-
-    // Generate api url with city and country name
-    const api = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=8b9fb0f997204e2487c49916e25f3c18`
-
-    // Fetching data via the api
-    fetch(api, {mode: 'cors'})
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        const {temp, humidity} = data.main;
-        const {description} = data.weather[0];
-        temperature.textContent = `${temp}K`;
-        desc.textContent = `${description}`;
-        hum.textContent = `Humidity: ${humidity}`;
-        document.querySelector('.datetime').textContent = date;
+    if (location.length > 0) {
+      city = location.match(cityRegEx).groups.city;
+      country = location.match(countryRegEx).groups.country;
 
 
-        if (description.includes("rain")) {
-          content.classList.add("rainyday")
-        } else if (description.includes("cloud")) {
-          content.classList.add("cloudyday")
-        } else {
-          content.classList.add("clearday");
-        }
+      // Clearing the input field after submit
+      document.querySelector('#location').value = '';
 
 
+      // Generate api url with city and country name
+      const api = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=8b9fb0f997204e2487c49916e25f3c18`
 
-      })
-      .catch(e => {
-        console.log(e);
-      })
+      // Fetching data via the api
+      fetch(api, {mode: 'cors'})
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          const {temp, humidity} = data.main;
+          const {description} = data.weather[0];
+          temperature.textContent = `${temp}K`;
+          desc.textContent = `${description}`;
+          hum.textContent = `Humidity: ${humidity}`;
+          document.querySelector('.datetime').textContent = today;
 
+          // Changing the background gif based on the weather conditions
+          if (description.includes("rain")) {
+            content.classList.add("rainyday")
+          } else if (description.includes("cloud")) {
+            content.classList.add("cloudyday")
+          } else {
+            content.classList.add("clearday");
+          }
 
+        })
+        .catch(e => {
+          console.log(e);
+        })
 
-
-
+    } else {
+      alert("Please input city, country");
+    }
 
   });
-
-
 
 
 })
